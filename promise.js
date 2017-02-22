3ebdc8fbfbd6e95399a6cc81281de4b057a05fd5
Edy Segura <ednardo@inatel.br>
2017-02-21 23:44:03 -0300

    Refactor in promise.js

--- promise.js
+++ promise.js
@@ -1,45 +1,45 @@
-var 
-	q = require('q'),
-	fs = require('fs');
+const q = require('q');
+const fs = require('fs');
 
 function readData(filename) {
-	var defer = q.defer();
-	fs.readFile(filename, 'utf8', function(err, content) {
-		var data = {filename: filename, content: content};
-		defer.resolve(data);
-		//when there is a error call the reject
-		//defer.reject(data);
-	});
-	return defer.promise;
+    let defer = q.defer();
+    fs.readFile(filename, 'utf8', function (err, content) {
+        if(err) defer.reject(err);
+        let data = { filename: filename, content: content };
+        defer.resolve(data);
+    });
+    return defer.promise;
 };
 
 function writeData(data) {
-	var defer = q.defer();
-	fs.writeFile(data.filename, data.content + " callback 1", function(err) {
-		defer.resolve(data);
-	});
-	return defer.promise;
+    let defer = q.defer();
+    fs.writeFile(data.filename, data.content + " callback 1", function (err) {
+        if(err) defer.reject(err);
+        defer.resolve(data);
+    });
+    return defer.promise;
 };
 
 function addToLog(data) {
-	var defer = q.defer();
-	fs.writeFile('myLog.txt', data.content + " callback 2", function(err) {
-		defer.resolve(data);
-	});
-	return defer.promise;
+    let defer = q.defer();
+    fs.writeFile('./data/myLog.txt', data.content + " callback 2", function (err) {
+        if(err) defer.reject(err);
+        defer.resolve(data);
+    });
+    return defer.promise;
 };
 
 function notifyMeOnSuccess(data) {
-	console.log('Process finished!');
+    console.log('Process finished!');
 };
 
 function notifyMeOnError(error) {
-	// Handle any error from all above steps
-    console.log('Process has been failed', error);
+    // Handle any error from all above steps
+    console.log('Process has been failed: ', error);
 };
 
-readData('myData.txt')
-.then(writeData)
-.then(addToLog)
-.then(notifyMeOnSuccess)
-.catch(notifyMeOnError);
+readData('./data/myData.txt')
+    .then(writeData)
+    .then(addToLog)
+    .then(notifyMeOnSuccess)
+    .catch(notifyMeOnError);
