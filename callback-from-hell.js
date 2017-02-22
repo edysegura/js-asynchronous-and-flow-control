1000586f9ea54b8bc502c98b4ff16e50e450c177
Edy Segura <ednardo@inatel.br>
2017-02-22 00:22:22 -0300

    Added error handling in callback-from-hell.js

--- callback-from-hell.js
+++ callback-from-hell.js
@@ -1,12 +1,14 @@
-var 
-	fs = require('fs'),
-	myDataFile = 'myData.txt',
-	myLogFile = 'myLog.txt';
+const fs = require('fs');
+const myDataFile = './data/myData.txt';
+const myLogFile = './data/myLog.txt';
 
-fs.readFile(myDataFile, 'utf8', function(err, data) {
-	fs.writeFile(myDataFile, data + " callback 1", function(err) {
-		fs.writeFile(myLogFile, data + " callback 2", function(err) {
-			console.log("Process finished!");
-		});
-	});
+fs.readFile(myDataFile, 'utf8', function (err, data) {
+    if(err) throw err;
+    fs.writeFile(myDataFile, data + " callback 1", function (err) {
+        if(err) throw err;
+        fs.writeFile(myLogFile, data + " callback 2", function (err) {
+            if(err) throw err;
+            console.log("Process finished!");
+        });
+    });
 });
