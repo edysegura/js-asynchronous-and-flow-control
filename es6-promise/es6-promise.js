198d21119774911d29085b9174911ee81aa52a8e
Edy Segura <ednardo@inatel.br>
2017-02-25 20:54:12 -0300

    Refactoring in es6-promise.js

--- es6-promise/es6-promise.js
+++ es6-promise/es6-promise.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 
 function readData(filename) {
     let promise = new Promise((resolve, reject) => {
-        fs.readFile(filename, 'utf8', function (err, content) {
-            if(err) reject(err);
+        fs.readFile(filename, 'utf8', (error, content) => {
+            if(error) reject(error);
             let data = { filename: filename, content: content };
             resolve(data);
         });
@@ -13,8 +13,8 @@ function readData(filename) {
 
 function writeData(data) {
     let promise = new Promise((resolve, reject) => {
-        fs.writeFile(data.filename, data.content + " callback 1", function (err) {
-            if(err) reject(err);
+        fs.writeFile(data.filename, data.content + "\nadded text", (error) => {
+            if(error) reject(error);
             resolve(data);
         });
     });
@@ -23,19 +23,20 @@ function writeData(data) {
 
 function addToLog(data) {
     let promise = new Promise((resolve, reject) => {
-        fs.writeFile('../data/myLog.txt', data.content + " callback 2", function (err) {
-            if(err) reject(err);
+        let logMessage = `[${new Date().toISOString()}] ${data.filename} has been changed`;
+        fs.writeFile('../data/myLog.txt', logMessage, (error) => {
+            if(error) reject(error);
             resolve(data);
         });
     });
     return promise;
 };
 
-function notifyMeOnSuccess(data) {
-    console.log('Process finished!');
+function notifyOnSuccess(data) {
+    console.log('Process has been finished!');
 };
 
-function notifyMeOnError(error) {
+function notifyOnError(error) {
     // Handle any error from all above steps
     console.log('Process has been failed: ', error);
 };
@@ -43,5 +44,5 @@ function notifyMeOnError(error) {
 readData('../data/myData.txt')
     .then(writeData)
     .then(addToLog)
-    .then(notifyMeOnSuccess)
-    .catch(notifyMeOnError);
+    .then(notifyOnSuccess)
+    .catch(notifyOnError);
