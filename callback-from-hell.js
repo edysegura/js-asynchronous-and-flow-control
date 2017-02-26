b3414145d6e9329eb6be7c0fa45aaa79961c7910
Edy Segura <ednardo@inatel.br>
2017-02-25 21:08:55 -0300

    Updated callback-from-hell.js

--- callback-from-hell.js
+++ callback-from-hell.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const myDataFile = './data/myData.txt';
 const myLogFile = './data/myLog.txt';
 
-fs.readFile(myDataFile, 'utf8', function (err, data) {
-    if(err) throw err;
-    fs.writeFile(myDataFile, data + " callback 1", function (err) {
-        if(err) throw err;
-        fs.writeFile(myLogFile, data + " callback 2", function (err) {
-            if(err) throw err;
-            console.log("Process finished!");
+fs.readFile(myDataFile, 'utf8', function (error, data) {
+    if(error) throw error;
+    fs.writeFile(myDataFile, data + " callback 1", function (error) {
+        if(error) throw error;
+        fs.writeFile(myLogFile, data + " callback 2", function (error) {
+            if(error) throw error;
+            console.log("Process has been finished!");
         });
     });
 });
