a5ec0d72a0aa3498515ed65b3d1bb63de03f399d
Edy Segura <ednardo@inatel.br>
2017-03-05 21:57:08 -0300

    Added async module example. This fixes #5

---
+++ async-module/README.md
@@ -0,0 +1,17 @@
+# Async Module
+
+This example is using [Async Module](http://caolan.github.io/async/index.html) to demonstrate the how to organize async operations.
+
+## How to run it
+
+Assuming you have [Node.js](https://nodejs.org/en/) installed just run the commands below:
+
+```
+npm install
+```
+
+and then
+
+```
+npm start
+```
---
+++ async-module/async-module.js
@@ -0,0 +1,38 @@
+const async = require('async');
+const fs = require('fs');
+
+function readData(callback) {
+    let filename = '../data/myData.txt';
+    fs.readFile(filename, 'utf8', (error, content) => {
+        if (error) callback(error);
+        let data = { filename: filename, content: content };
+        callback(null, data);
+    });
+};
+
+function writeData(data, callback) {
+    fs.writeFile(data.filename, data.content + "\nadded text", (error) => {
+        if (error) callback(error);
+        callback(null, data);
+    });
+};
+
+function addToLog(data, callback) {
+    let logMessage = `[${new Date().toISOString()}] ${data.filename} has been changed`;
+    fs.writeFile('../data/myLog.txt', logMessage, (error) => {
+        if (error) callback(error);
+        callback(null, data);
+    });
+};
+
+function notifyOnDone(error, result) {
+    // Handle any error from all above steps
+    if(error) console.log('Process has been failed: ', error);
+    console.log('Process has been finished!');
+};
+
+async.waterfall([
+    readData,
+    writeData,
+    addToLog
+], notifyOnDone);
