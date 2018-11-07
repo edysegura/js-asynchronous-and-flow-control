14e0ede1f1386ecc24c30d8ede2907fa51b55b03
Edy Segura <edysegura@gmail.com>
2018-11-06 23:25:10 -0200

    Refactoring async-await example

--- async-await/async-await.js
+++ async-await/async-await.js
@@ -1,15 +1,20 @@
-const { readData, writeData, addToLog, notifyOnSuccess, notifyOnError } = require('../commons/promises')
+const {
+  readData,
+  writeData,
+  addToLog,
+  notifyOnSuccess,
+  notifyOnError
+} = require('../commons/promises')
 
 async function main() {
-    try {
-        const fileContent = await readData('../data/myData.txt')
-        const result = await writeData(fileContent)
-        await addToLog(result)
-        notifyOnSuccess()
-    }
-    catch (error) {
-        notifyOnError(error)
-    }
+  try {
+    const fileContent = await readData('../data/myData.txt')
+    const result = await writeData(fileContent)
+    await addToLog(result)
+    notifyOnSuccess()
+  } catch (error) {
+    notifyOnError(error)
+  }
 }
 
 main()
