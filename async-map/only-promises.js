fd8cf36c70f719da0f17eb53482d171dd9a0baaf
Edy Segura <edysegura@gmail.com>
2018-11-12 22:50:38 -0200

    Added example with promises

---
+++ async-map/only-promises.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch')
+
+const baseEndpoint = 'https://swapi.co/api/people'
+const endpointList = [
+  `${baseEndpoint}/1/`,
+  `${baseEndpoint}/6/`,
+  `${baseEndpoint}/12/`,
+  `${baseEndpoint}/20/`
+]
+
+const requestHandler = endpoint => {
+  return new Promise(async resolve => {
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    resolve(data)
+  })
+}
+
+async function showNames(endpointList) {
+  const fetchData = endpointList.map(requestHandler)
+  const fetchedData = await Promise.all(fetchData)
+  const characters = fetchedData.map(data => data.name)
+  console.log(characters)
+}
+
+showNames(endpointList)
