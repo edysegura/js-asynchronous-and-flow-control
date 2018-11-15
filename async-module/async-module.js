199b1e64732831d568d74a3a1dfc4a3e036a10c3
Edy Segura <edysegura@gmail.com>
2018-11-15 16:11:11 -0200

    Refactoring async module example

--- async-module/async-module.js
+++ async-module/async-module.js
@@ -1,38 +1,41 @@
-const async = require('async');
-const fs = require('fs');
+const async = require('async')
+const fs = require('fs')
 
 function readData(callback) {
-    let filename = '../data/myData.txt';
-    fs.readFile(filename, 'utf8', (error, content) => {
-        if (error) callback(error);
-        let data = { filename: filename, content: content };
-        callback(null, data);
-    });
-};
+  let filename = '../data/myData.txt'
+  fs.readFile(filename, 'utf8', (error, content) => {
+    if (error) callback(error)
+    let data = { filename, content }
+    callback(null, data)
+  })
+}
 
 function writeData(data, callback) {
-    fs.writeFile(data.filename, data.content + "\nadded text", (error) => {
-        if (error) callback(error);
-        callback(null, data);
-    });
-};
+  fs.writeFile(data.filename, data.content + '\nadded text', error => {
+    if (error) callback(error)
+    callback(null, data)
+  })
+}
 
 function addToLog(data, callback) {
-    let logMessage = `[${new Date().toISOString()}] ${data.filename} has been changed`;
-    fs.writeFile('../data/myLog.txt', logMessage, (error) => {
-        if (error) callback(error);
-        callback(null, data);
-    });
-};
+  let logMessage = `[${new Date().toISOString()}] ${data.filename} has been changed`
+  fs.writeFile('../data/myLog.txt', logMessage, error => {
+    if (error) callback(error)
+    callback(null, data)
+  })
+}
 
-function notifyOnDone(error, result) {
-    // Handle any error from all above steps
-    if(error) console.log('Process has been failed: ', error);
-    console.log('Process has been finished!');
-};
+function notifyOnDone(error) {
+  // Handle any error from all above steps
+  if (error) console.log('Process has been failed: ', error)
+  console.log('Process has been finished!')
+}
 
-async.waterfall([
+async.waterfall(
+  [
     readData,
     writeData,
     addToLog
-], notifyOnDone);
+  ],
+  notifyOnDone
+)
