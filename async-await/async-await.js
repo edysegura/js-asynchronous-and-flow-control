5e3037547adcdc0fe114a536a41c16a0758145a9
Edy Segura <edysegura@gmail.com>
2019-08-05 16:47:20 -0300

    Update async/await example

--- async-await/async-await.js
+++ async-await/async-await.js
@@ -1,20 +1,43 @@
-const {
-  readData,
-  writeData,
-  addToLog,
-  notifyOnSuccess,
-  notifyOnError
-} = require('../commons/promises')
+const { promisify } = require('util');
+const fs = require('fs');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+function readData(filename) {
+  const createMessage = data => ({ filename, data });
+  return readFile(filename, 'utf-8').then(createMessage);
+}
+
+function writeData(file) {
+  const data = file.data + '\nnew content';
+  const createMessage = () => ({ filename: file.filename });
+  return writeFile(file.filename, data).then(createMessage);
+}
+
+function addToLog(data) {
+  const logMessage = `${data.filename} has been changed`
+  return writeFile('my-log.txt', logMessage)
+}
+
+function notifyOnSuccess() {
+  console.log('Process has been finished!')
+}
+
+function notifyOnError(error) {
+  console.log('Process has been failed: ')
+  console.log(error.message)
+}
 
 async function main() {
   try {
-    const fileContent = await readData('../data/myData.txt')
-    const result = await writeData(fileContent)
-    await addToLog(result)
-    notifyOnSuccess()
+    const file = await readData('my-file.txt');
+    const result = await writeData(file);
+    await addToLog(result);
+    notifyOnSuccess();
   } catch (error) {
-    notifyOnError(error)
+    notifyOnError(error);
   }
 }
 
-main()
+main();
