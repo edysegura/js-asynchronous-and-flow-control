b04d756f0c996ecb8481de752fe82ed882e244f4
Edy Segura <edysegura@gmail.com>
2019-08-12 21:43:35 -0300

    Update indentation

--- es6-promise-all/es6-promise-all.js
+++ es6-promise-all/es6-promise-all.js
@@ -1,17 +1,17 @@
 const fs = require('fs')
 
 function stats(file) {
-    return new Promise((resolve, reject) => {
-        fs.stat(file, (error, data) => {
-            if (error) reject(error)
-            else resolve(data)
-        })
+  return new Promise((resolve, reject) => {
+    fs.stat(file, (error, data) => {
+      if (error) reject(error)
+      else resolve(data)
     })
+  })
 }
 
 Promise.all([
-    stats('../data/myData.txt'),
-    stats('../data/myLog.txt')
+  stats('../data/myData.txt'),
+  stats('../data/myLog.txt')
 ])
-.then(data => console.log(data))
-.catch(error => console.log(error))
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
