ad39a7900b7d9e86f16129f120de13498eead5fa
Edy Segura <edysegura@gmail.com>
2021-06-29 22:16:18 -0300

    Fix file path

--- top-level-await/index.mjs
+++ top-level-await/index.mjs
@@ -1,7 +1,7 @@
 import { readFile } from 'fs/promises'
 
 // top level await is available since Node.js v14.8
-const filePath = new URL('../myData.txt', import.meta.url)
+const filePath = new URL('../data/myData.txt', import.meta.url)
 const fileContent = await readFile(filePath, 'utf8')
 
 console.log(fileContent)
