05fd95073861e03c18829dd38fb31c6634c1b3fe
Edy Segura <edysegura@gmail.com>
2021-07-14 22:28:57 -0300

    fetch first level of multiple requests

--- multiple-requests/index.js
+++ multiple-requests/index.js
@@ -13,3 +13,13 @@
 * ]
 *
 */
+
+import axios from 'axios'
+
+async function getPokemonData(pokeId) {
+  const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokeId}`
+  const response = await axios.get(endpoint)
+  return response.data.name
+}
+
+console.log(await getPokemonData(1))
