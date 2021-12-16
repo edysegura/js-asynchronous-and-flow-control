db18715f9c5b7d86f1b62cd73950eefb652ff433
Edy Segura <edysegura@gmail.com>
2021-12-15 23:21:02 -0300

    code formatting

--- multiple-requests/index.js
+++ multiple-requests/index.js
@@ -1,30 +1,29 @@
 /**
-* APIs:
-* - https://pokeapi.co/api/v2/pokemon/{pokeId}
-* - https://pokeapi.co/api/v2/pokemon-species/{specieId}
-* - https://pokeapi.co/api/v2/evolution-chain/{evolutionChainId}
-*
-* Output:
-* [
-*  {
-*    "name": "bulbasaur",
-*    "types": [
-*      "grass",
-*      "poison"
-*    ],
-*    "envolvesTo": "ivysaur"
-*  },
-* ]
-*
-*/
+ * APIs:
+ * - https://pokeapi.co/api/v2/pokemon/{pokeId}
+ * - https://pokeapi.co/api/v2/pokemon-species/{specieId}
+ * - https://pokeapi.co/api/v2/evolution-chain/{evolutionChainId}
+ *
+ * Output:
+ * [
+ *  {
+ *    "name": "bulbasaur",
+ *    "types": [
+ *      "grass",
+ *      "poison"
+ *    ],
+ *    "envolvesTo": "ivysaur"
+ *  },
+ * ]
+ *
+ */
 
-import axios from 'axios'
-
+import axios from "axios";
 
 async function getPokemonData(pokeId) {
-  const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokeId}`
-  const response = await axios.get(endpoint)
-  return response.data.name
+  const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokeId}`;
+  const response = await axios.get(endpoint);
+  return response.data.name;
 }
 
-console.log(await getPokemonData(1))
+console.log(await getPokemonData(1));
